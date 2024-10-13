e0a82288e7ca9cd08fd8f8d172eed020627f0c31
Kanyapa2538 <65010912538@msu.ac.th>
2024-10-13 18:55:53 +0700

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const postController = require('../controllers/postController');
+const productController = require('../controllers/productController');
+const productDetailController = require('../controllers/product_detailController');
 
 // User routes
 router.post('/users', userController.createUser);
@@ -18,5 +20,19 @@ router.get('/posts', postController.getPosts);
 router.get('/posts/:id', postController.getPostById);
 router.put('/posts/:id', postController.updatePost);
 router.delete('/posts/:id', postController.deletePost);
+
+// Product routes
+router.post('/products', productController.createProduct);
+router.get('/products', productController.getProducts);
+router.get('/products/:id', productController.getProductById);
+router.put('/products/:id', productController.updateProduct);
+router.delete('/products/:id', productController.deleteProduct);
+
+// Product_detail routes
+router.post('/products/:productId/details', productDetailController.createProductDetail);
+router.get('/products/:productId/details', productDetailController.getProductDetails);
+router.get('/details/:id', productDetailController.getProductDetailById);
+router.put('/details/:id', productDetailController.updateProductDetail);
+router.delete('/details/:id', productDetailController.deleteProductDetail);
 
 module.exports = router;
