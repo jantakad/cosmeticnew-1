b2b6680a14dc0912f2d1136897ec634c604347e6
Kanyapa2538 <65010912538@msu.ac.th>
2024-10-29 01:04:41 +0700

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,7 +61,7 @@ res.json({ message: 'User deleted' });
 res.status(500).json({ error: err.message });
 }
 };
-// 
+// login ด้วย email และ password
 exports.login = async (req, res) => {
     try {
     const user = await User.findOne(
